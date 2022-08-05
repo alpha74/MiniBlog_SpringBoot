69d1ea2157587bd5c0a1ce9219f10f7e2435263c
alpha74 <amanalphakumar@gmail.com>
2022-08-05 16:43:26 +0530

    Initial commit

---
+++ src/main/java/com/alpha74/rest/webserver/springrestweb/SpringRestWebApplication.java
@@ -0,0 +1,13 @@
+package com.alpha74.rest.webserver.springrestweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpha74/rest/webserver/springrestweb/SpringRestWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha74.rest.webserver.springrestweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
