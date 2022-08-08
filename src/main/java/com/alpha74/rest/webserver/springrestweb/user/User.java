60a50b044e000f05610aff388f77b90e1306b7f7
alpha74 <amanalphakumar@gmail.com>
2022-08-08 17:19:52 +0530

    Add Validation to createUser() API

--- src/main/java/com/alpha74/rest/webserver/springrestweb/user/User.java
+++ src/main/java/com/alpha74/rest/webserver/springrestweb/user/User.java
@@ -1,11 +1,20 @@
 package com.alpha74.rest.webserver.springrestweb.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User
 {
     private Integer id ;
+
+    /*
+        Using 'message' key will add a default message in response body.
+     */
+    @Size(min=2, message = "Name should have atleast 2 characters")
     private String name ;
+
+    @Past
     private Date birthDate ;
 
     protected User()
