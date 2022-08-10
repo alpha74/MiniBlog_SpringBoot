dfb37fafe666ada3fa38058244846a311c3a0a62
alpha74 <amanalphakumar@gmail.com>
2022-08-10 14:12:33 +0530

    Add Internalization in healthStatusi18n() API.

--- src/main/java/com/alpha74/rest/webserver/springrestweb/healthStatus/HealthStatusController.java
+++ src/main/java/com/alpha74/rest/webserver/springrestweb/healthStatus/HealthStatusController.java
@@ -1,11 +1,21 @@
 package com.alpha74.rest.webserver.springrestweb.healthStatus;
 
 import com.alpha74.rest.webserver.springrestweb.healthStatus.HealthStatusBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HealthStatusController
 {
+    @Autowired
+    /*
+        MessageSource is a feature provided by Spring to pick up message bundles.
+     */
+    private MessageSource messageSource;
     /*
         This method returns a string
      */
@@ -13,6 +23,21 @@ public class HealthStatusController
     public String healthStatus()
     {
         return "status:OK" ;
+    }
+
+    /*
+        Implementing Internalization
+
+        - This take Header "Accept-Language" in the request.
+        _ Instead of hard-coding, we are using 'messages.properties' file.
+     */
+    @GetMapping(path="/healthStatus/i18n")
+    public String healthStatusi18n(
+            @RequestHeader(name="Accept-Language", required = false)
+            Locale locale)
+    {
+        return messageSource.
+                getMessage("health.status.message", null, "OK:Default", LocaleContextHolder.getLocale());
     }
 
     /*
